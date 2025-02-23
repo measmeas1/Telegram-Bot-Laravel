7543eb3d87e50df986d709786c83e572579c3c61
measmeas1 <mabmeas12@gmail.com>
2025-02-23 12:57:01 +0700

    train bot

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -9,17 +9,38 @@ class TelegramBotController extends Controller
 {
     public function handleWebhook(Request $request)
     {
-      $update = Telegram::commandsHandler(true);
-      return response()->json(['status' => 'ok']);
+        \Log::info($request->all());
+
+        // Get message details
+        $chat_id = $request->message['from']['id'];
+        $reply_to_message = $request->message['message_id'];
+        $message_text = $request->message['text'] ?? '';
+
+        \Log::info("chat_id: {$chat_id}");
+        \Log::info("reply_to_message: {$reply_to_message}");
+
+        // Check if the user said 'hi bot'
+        if (strtolower($message_text) === 'hi bot') {
+            // Send a message back to the user
+            Telegram::sendMessage([
+                'chat_id' => $chat_id,
+                'reply_to_message_id' => $reply_to_message,
+                'text' => 'I love you oun nith. 🥰😜'
+            ]);
+        }
+
+        return 'Webhook handled successfully';
     }
 
-    public function sendMessage()
-    {
-        Telegram::sendMessage([
-            'chat_id' => 'your_chat_id',
-            'text' => 'Hello from Laravel Telegram Bot!'
-        ]);
+    // public function sendMessage()
+    // {
+    //     Telegram::sendMessage([
+    //         'chat_id' => 'your_chat_id',
+    //         'text' => 'Hello from Laravel Telegram Bot!'
+    //     ]);
 
-        return 'Message sent!';
-    }
+    //     return 'Message sent!';
+    // }
 }
+
+
